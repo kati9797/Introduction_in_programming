48b2da08d40c818ffbc293931e6db6f78df9cce8
Teodor Karakashev <teodor.karakashev@gmail.com>
2022-08-05 22:32:51 +0300

    bestCodeEverWriten

---
+++ bestCodeEverWritten.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+//function that says "Github - copilot is the best thing ever!"
+void github_copilot()
+{
+	std::cout << "Github - copilot is the best thing ever!" << std::endl;
+}
+
+int main() {
+	while (true)
+	{
+		github_copilot();
+	}
+}
