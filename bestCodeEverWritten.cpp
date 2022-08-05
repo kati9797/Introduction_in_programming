557d6e36c14078bb6ac47a053e9aaf9a95d49b82
Teodor Karakashev <teodor.karakashev@gmail.com>
2022-08-05 22:34:38 +0300

    update on best code ever

--- bestCodeEverWritten.cpp
+++ bestCodeEverWritten.cpp
@@ -6,6 +6,12 @@ void github_copilot()
 	std::cout << "Github - copilot is the best thing ever!" << std::endl;
 }
 
+//functon that says "Кати ще си вземе всички изпити от поправката!"
+void kati_s_vzeto_vse_izpiti_ot_popravka()
+{
+	std::cout << "Кати ще си вземе всички изпити от поправката!" << std::endl;
+}
+
 int main() {
 	while (true)
 	{
