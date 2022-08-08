ab379d99a19c77597f8a43d1780f8b0080cbafb5
Ekaterina <atanasova150@gmail.com>
2022-08-08 22:07:19 +0300

    Update Path_in_maze.cpp

--- Recursion/Path_in_maze.cpp
+++ Recursion/Path_in_maze.cpp
@@ -1,24 +1,36 @@
-#include <iostream>
 const int N = 128;
 
 bool isSafe(int maze[N][N], int n, int m, int x, int y)
 {
-    return (x >= 0 && x < n && y >= 0 && y < m) && (maze[x][y] == 0);
+    return (x >= 0 && x < n&& y >= 0 && y < m) && (maze[x][y] == 0);
 }
 
-bool solve(int maze[N][N], int n, int m, int sx, int sy, int dx, int dy)
+void solve(int maze[N][N], int n, int m, int sx, int sy, int dx, int dy)
 {
     if (sx == dx && sy == dy)
-        return true;
+    {
+        maze[sx][sy] = 1; 
+        std::cout << "--------------" << std::endl;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                std::cout << maze[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
+        return;
+    }
 
-        if (!isSafe(maze, n, m, sx, sy))
-            return false; 
+    if (!isSafe(maze, n, m, sx, sy))
+        return;
 
-        maze[sx][sy] = 1;
-        return solve(maze, n, m, sx + 1, sy, dx, dy) ||
-               solve(maze, n, m, sx - 1, sy, dx, dy) ||
-               solve(maze, n, m, sx, sy + 1, dx, dy) ||
-               solve(maze, n, m, sx, sy - 1, dx, dy);
+    maze[sx][sy] = 1;
+    solve(maze, n, m, sx + 1, sy, dx, dy);
+    solve(maze, n, m, sx - 1, sy, dx, dy);
+    solve(maze, n, m, sx, sy + 1, dx, dy);
+    solve(maze, n, m, sx, sy - 1, dx, dy);
+    maze[sx][sy] = 0;
 }
 
 int main()
@@ -36,19 +48,13 @@ int main()
 
     int sx, sy, dx, dy;
     std::cin >> sx >> sy >> dx >> dy;
-    if (solve(maze, n, m, sx, sy, dx, dy))
-    {
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                std::cout<<maze[i][j]<<" ";
-            }
-            std::cout << std::endl;
-        }
-    }
-    else
-    {
-        std::cout << "No path";
-    }
+    solve(maze, n, m, sx, sy, dx, dy);
 }
+
+/*
+3 4
+0 0 1 1
+0 0 0 1
+1 0 0 0
+0 0 2 3
+*/
